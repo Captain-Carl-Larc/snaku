e448ba0e5c69d15183548ad9b670e77d3eae8551
Captain-Carl-Larc <155651529+Captain-Carl-Larc@users.noreply.github.com>
2024-12-21 20:41:42 +0300

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,16 @@
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div className="text-3xl font-bold">
+      <h2>
+        SNAKU OFFICIAL
+      </h2>    
+      <nav>
+        <ol>
+          <li>
+            
+          </li>
+        </ol>
+      </nav>  
+    </div>
   )
 }
